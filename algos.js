5ea1f70aad4c0c4a331f071557d2322a6b5296a9
johnloberger <johnloberger@gmail.com>
2020-09-17 23:32:52 -0500

    valid anagram detector

--- algos.js
+++ algos.js
@@ -51,3 +51,30 @@ function charCount(str) {
   }
   return result;
 }
+
+
+// anagram valid?
+
+function validAnagram(first, second) {
+  if (first.length !== second.length){
+    return false
+  }
+  const lookup = {};
+
+  for (let i=0;i <first.length; i++){
+    let letter = first[i];
+    // if letter exists, increment, otherwise set to 1
+    lookup[letter] ? lookup[letter] += 1 : lookup[letter] = 1
+  }
+
+  for (let i=0; i <second.length; i++){
+    let letter = second[i]
+    // can't find second letter or letter is zero then its not anagram
+    if (!lookup[letter]){
+      return false
+    } else {
+      lookup[letter] -= 1
+    }
+  }
+  return true
+}
