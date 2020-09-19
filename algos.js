c87d92a5e3e454779c523b71702b9aab25d4287d
johnloberger <johnloberger@gmail.com>
2020-09-18 23:45:37 -0500

    sum zero algo

--- algos.js
+++ algos.js
@@ -78,3 +78,21 @@ function validAnagram(first, second) {
   }
   return true
 }
+
+// accept a sorted array of integers and find the first pair where the sum is 0
+
+function sumZero(arr){
+  let left = 0
+  let right = arr.length - 1;
+  while(left < right){
+    let sum = arr[left] + arr[right]
+    if(sum === 0){
+      return [arr[left], arr[right]]
+    } else if (sum > 0){
+      right--;
+    } else {
+      left++;
+    }
+  }
+}
+
