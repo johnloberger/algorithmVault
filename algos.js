d4f2bb63d1ca770ce81dc8f51d7f81540a17dd68
johnloberger <johnloberger@gmail.com>
2020-09-22 22:14:17 -0500

    count unique values algo

--- algos.js
+++ algos.js
@@ -96,3 +96,15 @@ function sumZero(arr){
   }
 }
 
+// accept a sorted array and count the unique values, even negatives
+// [1,1,2] returns 2
+function countUniqueValues(arr){
+  let i = 0;
+  for (let j = 1; j < arr.length; j++){
+    if(arr[i] !== arr[j]){
+      i++;
+      arr[i] = arr[j]
+    }
+    console.log(i,j)
+  }
+}
