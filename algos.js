270e4c3203d96169e9d760c5db36942be555e029
johnloberger <johnloberger@gmail.com>
2020-09-26 23:04:17 -0500

    search for index of given value algo

--- algos.js
+++ algos.js
@@ -108,3 +108,23 @@ function countUniqueValues(arr){
     console.log(i,j)
   }
 }
+
+// given a sorted array of integers, write a function that accepts a value and returns the index where the value is located
+
+function search(array, val){
+  let min = 0
+  let max = array.length -1
+
+  while (min <= max) {
+    let middle = Math.floor((min + max) / 2)
+    let currentElement = array[middle];
+    if (array[middle] < val){
+      max = middle - 1;
+    } else if (array[middle] > val){
+      max = middle - 1;
+    } else {
+      return middle;
+    }
+  }
+  return -1;
+}
